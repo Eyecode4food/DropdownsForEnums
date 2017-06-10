8e1c97400f0c50833c1aeb0022659273eef0ad37
tnayanam <nayatanuj@gmail.com>
2017-06-10 02:33:51 -0400

    Added dropdown for enums

---
+++ DropdownFromEnum/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DropdownFromEnum.Startup))]
+namespace DropdownFromEnum
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
